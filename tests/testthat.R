053912ce50e848501603bc74c4ea6416a5e2afb0
OlehYashchuk <OlehYashchuk@gmail.com>
2019-05-13 18:46:20 +0300

    Documentation and test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(fita)
+
+test_check("fita")
---
+++ tests/testthat/test_make_filename.R
@@ -0,0 +1,7 @@
+library(testthat)
+
+test_that('check filename is created properly', {
+  filename_2013 <- make_filename(2013)
+  
+  expect_that(filename_2013, is_a('character'))
+})
